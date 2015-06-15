8842a68dbaa1bee205a88fdda501cef2eececb39
BR developer <ajay@brsoftech.com>
2015-06-15 16:05:55 +0530

    css issue

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,8 @@ require 'date'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module RailsDevise
   class Application < Rails::Application
@@ -33,7 +34,7 @@ module RailsDevise
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-     config.serve_static_assets = false
+    # config.serve_static_assets = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
