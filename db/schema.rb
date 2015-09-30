ddb7583d9ead868d5fc6ac277705b85449a13d6b
Snuggs <rashaunstovall@gmail.com>
2015-09-30 00:20:54 -0400

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727100641) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20150813044623) do
 
   create_table "patients", force: :cascade do |t|
     t.date    "startdate"
@@ -26,10 +23,9 @@ ActiveRecord::Schema.define(version: 20150727100641) do
     t.string  "languagereading"
     t.string  "computer"
     t.string  "computerskills"
-    t.string  "topicdiscussed"
+    t.string  "navigator"
     t.string  "timelength"
     t.string  "notes"
-    t.string  "noofparticipant"
     t.string  "noofinteraction"
     t.string  "location"
     t.integer "userid"
@@ -55,7 +51,7 @@ ActiveRecord::Schema.define(version: 20150727100641) do
     t.boolean  "status",                 default: true
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
